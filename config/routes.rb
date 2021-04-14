7f50bd428b9fefd1ee15c402bdb9f5e09ac0b903
Govind Upadhyay <govindupadhyay@startbitsolutions.com>
2021-04-14 12:49:14 +0530

    Restrict actions, views and

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
-  resources :students, except: [:destroy]
+  resources :students, except: [:new, :destroy]
+  get 'signup', to: 'students#new'
   get 'login', to: 'logins#new'
   post 'login', to: 'logins#create'
   delete 'logout', to: 'logins#destroy'
+  post 'course_enroll', to: 'student_courses#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
